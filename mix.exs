f0fff6e86c1cec76a55fc6b2d110e99f88e6b8c9
doorgan <dorgandash@gmail.com>
2020-09-11 20:10:53 -0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YarnParser.MixProject do
   def project do
     [
       app: :yarn_parser,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: "A simple yarn.lock parser",
